37693cf8dd3c84d7f0f0ab18d27c4ef7ea77ed87
argoneus <argoneuscze@gmail.com>
2019-04-03 13:32:25 +0200

    added basic HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Layout from "../containers/layout";
+import {HashRouter, Route} from "react-router-dom";
+import SearchResults from "./item_search/search_results";
+import ItemWatcher from "./item_watcher/item_watcher";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <HashRouter>
+                    <Route path="/" exact component={ItemWatcher}/>
+                    <Route path="/search" exact component={SearchResults}/>
+                </HashRouter>
+            </Layout>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/item_watcher/item_watcher.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class ItemWatcher extends React.Component {
+    render() {
+        return (
+            "watcher"
+        );
+    }
+}
