e73d8753760a64ee9de0c533509ba7e351b668df
argoneus <argoneuscze@gmail.com>
2019-04-03 21:18:31 +0200

    added local storage persistence

--- src/store.js
+++ src/store.js
@@ -1,8 +1,20 @@
 import {applyMiddleware, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
+import {loadState, saveState} from "./localStorage";
+import {throttle} from "lodash";
 
-export default createStore(
+const persistedState = loadState();
+const store = createStore(
     rootReducer,
+    persistedState,
     applyMiddleware(thunk)
 );
+
+store.subscribe(throttle(() => {
+    saveState({
+        watchlist: store.getState().watchlist
+    });
+}));
+
+export default store;
